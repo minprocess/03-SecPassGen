3cf1fefc4d17cc74d4cacdb404fe801fd5e21ec8
Bill Pate <wtpate69@hotmail.com>
2021-03-19 12:17:30 -0400

    Fix problem choosing uppercase letters and fix problem generator right number of letters in password

--- script.js
+++ script.js
@@ -10,44 +10,50 @@ function writePassword() {
 }
 
 function generatePassword() {
-  let UseLC = document.getElementById("checkIdLC");
-  let UseUC = document.getElementById("checkIdUC");
-  let UseSC = document.getElementById("checkIdSC");
-  let UseNum = document.getElementById("checkIdNum");
-  let NumChar = document.getElementById("textIdNum");
-  console.log("UseLC", UseLC.checked);
-  console.log("UseUC", UseUC.checked);
-  console.log("UseSC", UseSC.checked);
-  console.log("UseNum", UseNum.checked);
-  console.log("NumChar", NumChar.value);
+  /************* User Inputs to control what is in password ************* */
+  let useLowerCase = document.getElementById("checkIdLC");
+  let useUpperCase = document.getElementById("checkIdUC");
+  let useSpecialChars = document.getElementById("checkIdSC");
+  let useNumChars = document.getElementById("checkIdNC");
+  let NumOfChars = document.getElementById("textIdNum"); // Number of characters in password
+  console.log("    ");
+  console.log("    ");
+  console.log("Secure Password Generator");
+  console.log("useLowerCase checkbox state", useLowerCase.checked);
+  console.log("useUpperCase checkbox state", useUpperCase.checked);
+  console.log("useSpecialChars checkbox state", useSpecialChars.checked);
+  console.log("useNumChars checkbox state", useNumChars.checked);
+  console.log("NumChar", NumOfChars.value);  // number of characters in password
+
+  /************* Create array of characters to use in password ************* */
   var charset = [];
-  if (UseLC.checked) {
-    var lc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    charset = charset.concat(lc);
-    console.log("In generatePassword, UseLC is checked, charset.length = ",charset.length)
+  if (useLowerCase.checked) {
+    var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+    //var lc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    charset = charset.concat(lowercase);
+    console.log("In generatePassword, useLowerCase is checked, charset.length = ",charset.length)
   }
-  if (UseUC.checked) {
-    var uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    charset = charset.concat(uc);
-    console.log("In generatePassword, UseUC is checked, charset.length = ",charset.length)
+  if (useUpperCase.checked) {
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    charset = charset.concat(uppercase);
+    console.log("In generatePassword, useUpperCase is checked, charset.length = ",charset.length)
   }
-  if (UseSC.checked) {
-    var sc = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-    charset = charset.concat(sc);
-    console.log("In generatePassword, UseSC is checked, charset.length = ",charset.length)
+  if (useSpecialChars.checked) {
+    var specialChars = "`~!@#$%^&*()-_=+".split("");
+    charset = charset.concat(specialChars);
+    console.log("In generatePassword, useSpecialChars is checked, charset.length = ",charset.length)
   }
-  if (UseNum.checked) {
-    var numb = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-    charset = charset.concat(numb);
-    console.log("In generatePassword, UseNum is checked, charset.length = ",charset.length)
+  if (useNumChars.checked) {
+    var numChars = "1234567890".split("");
+    charset = charset.concat(numChars);
+    console.log("In generatePassword, useNumChars is checked, charset.length = ",charset.length)
   }
 
   let num = charset.length;
   let passw = '';
   if (num > 0)
   {
-    for (var i=0; i<NumChar.value-1; i++)
+    for (var i=0; i<NumOfChars.value; i++)
     {
       // Get random index from array of options
       var index = Math.floor(Math.random() * num);
@@ -60,7 +66,3 @@ function generatePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-console.log("length of a ", a.length)
-console.log("lc[0] = ", lc[0]);
-console.log("lc[", a.length-1, "] = ",lc[a.length-1]);
