0e0b57c85796b3aa6d9913bfbe19476aea56066c
Bill Pate <wtpate69@hotmail.com>
2021-03-20 22:01:45 -0400

    Enable button if number is greater than 7

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let useLowerCase = document.getElementById("checkIdLC");
+let useUpperCase = document.getElementById("checkIdUC");
+let useSpecialChars = document.getElementById("checkIdSC");
+let useNumChars = document.getElementById("checkIdNC");
+let numOfChars = document.getElementById("textIdNum"); // Number of characters in password
 
 // Write password to the #password input
 function writePassword() {
@@ -11,11 +16,6 @@ function writePassword() {
 
 function generatePassword() {
   /************* User Inputs to control what is in password ************* */
-  let useLowerCase = document.getElementById("checkIdLC");
-  let useUpperCase = document.getElementById("checkIdUC");
-  let useSpecialChars = document.getElementById("checkIdSC");
-  let useNumChars = document.getElementById("checkIdNC");
-  let NumOfChars = document.getElementById("textIdNum"); // Number of characters in password
   console.log("    ");
   console.log("    ");
   console.log("Secure Password Generator");
@@ -23,37 +23,32 @@ function generatePassword() {
   console.log("useUpperCase checkbox state", useUpperCase.checked);
   console.log("useSpecialChars checkbox state", useSpecialChars.checked);
   console.log("useNumChars checkbox state", useNumChars.checked);
-  console.log("NumChar", NumOfChars.value);  // number of characters in password
+  console.log("NumChar", numOfChars.value);  // number of characters in password
 
   /************* Create array of characters to use in password ************* */
   var charset = [];
   if (useLowerCase.checked) {
     var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
-    //var lc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     charset = charset.concat(lowercase);
-    console.log("In generatePassword, useLowerCase is checked, charset.length = ",charset.length)
   }
   if (useUpperCase.checked) {
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     charset = charset.concat(uppercase);
-    console.log("In generatePassword, useUpperCase is checked, charset.length = ",charset.length)
   }
   if (useSpecialChars.checked) {
     var specialChars = "`~!@#$%^&*()-_=+".split("");
     charset = charset.concat(specialChars);
-    console.log("In generatePassword, useSpecialChars is checked, charset.length = ",charset.length)
   }
   if (useNumChars.checked) {
     var numChars = "1234567890".split("");
     charset = charset.concat(numChars);
-    console.log("In generatePassword, useNumChars is checked, charset.length = ",charset.length)
   }
 
   let num = charset.length;
   let passw = '';
   if (num > 0)
   {
-    for (var i=0; i<NumOfChars.value; i++)
+    for (var i=0; i<numOfChars.value; i++)
     {
       // Get random index from array of options
       var index = Math.floor(Math.random() * num);
@@ -63,6 +58,16 @@ function generatePassword() {
   return passw;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// If there is a keyup event for the text box with number of characters in password
+// Enable the generate password button if the number of characters is greater than it
+function EnableButton() {
+  console.log("Enable button numOfChars.value: ", numOfChars.value)
+  if (numOfChars.value > 7) {
+    generateBtn.disabled = false;
+  }
+}
 
+// Add event listener to button that causes password to be generate
+generateBtn.disabled = true;
+generateBtn.addEventListener("click", writePassword);
+numOfChars.addEventListener("keyup", EnableButton)
